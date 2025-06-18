5b9e8baeb517daef3f5c188732378ab2cbbbe595
Erasmo Junior <essj@ic.ufal.br>
2025-06-18 18:37:25 -0300

    feat: Distribuir os dados, treinar, testar e avaliar o modelo.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from sklearn import datasets
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Carregando o Iris Dataset
 iris_ds = datasets.load_iris()
@@ -29,3 +32,23 @@ plt.show()
 # Gráfico de dispersão entre todos os dados
 sb.pairplot(ds, hue='species')
 plt.show()
+
+# Definindo a distruibuição dos dados
+X = ds.drop('species', axis=1)
+y = ds['species']
+
+X_train, X_test, y_train, y_tests = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Treinando o modelo DecisionTreeClassfier
+model = DecisionTreeClassifier()
+model.fit(X=X_train, y=y_train)
+
+# Testando o modelo
+y_predict = model.predict(X=X_test)
+
+# Medindo a precisão do modelo
+accuracy = accuracy_score(y_true=y_tests, y_pred=y_predict)
+conf_matrix = confusion_matrix(y_true=y_tests, y_pred=y_predict)
+
+print(f"{100 * accuracy:.2f}%")
+print(conf_matrix)
